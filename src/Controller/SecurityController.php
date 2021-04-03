802acd7587c27472d22db8ed2743aa5cd9c1253d
christophe <castet.ch@hotmail.fr>
2021-04-03 23:22:57 +0200

    fix ( login ) : change provider to security yaml

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Message;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -15,6 +16,9 @@ class SecurityController extends AbstractController
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastusername = $authenticationUtils->getLastUsername();
+       /*  if($error['message'] ===  "Bad credentials.") {
+            $error['message'] = 'Identifiant ou mot de passe invalides';
+        } */
         return $this->render('security/index.html.twig', [
             'last_username' => $lastusername,
             'error' => $error
